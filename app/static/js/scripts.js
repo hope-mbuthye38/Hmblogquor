a63031616654ae88157b5b4aa2d82aa2a67035a4
hope-mbuthye38 <-C>
2021-09-29 01:46:51 +0300

    script

---
+++ app/static/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#profbutton").click(function(){
+      $("#profbutton").slideDown('1500').hide('1000');
+      $("#form").show('1500');
+    });
+    $("#submit").click(function(){
+      $("#profbutton").slideUp('1500');
+      $("#form").slideDown('1500');
+    });
+  }); 
+  $("#showform").click(function(){
+    $("#profileupdate").slideDown('2500');
+  });
+  $("#submit").click(function(){
+    $("#profileupdate").slideUp('1500');
+
+  });
+
+
