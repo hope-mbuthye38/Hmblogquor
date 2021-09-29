5ee80832fc8e6eb11548883506385d0dc65e8b7d
hope-mbuthye38 <-C>
2021-09-29 03:00:10 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -1,16 +1,92 @@
-from . import db
-
-#...
-
-class User(db.Model):
+from . import db,login_manager
+from flask_login import current_user,UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'User {self.name}'
+class User (UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    bio = db.Column(db.string(255))
-    profile_pic_path = db.Column(db.string(255))
-    password = db.Column (db.string(255))
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email = db.Column(db.String(255), unique = True,nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    profile_pic_path = db.Column(db.String(150),default ='default.png')
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    hashed_password = db.Column(db.String(255),nullable = False)
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
     
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @set_password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     def __repr__(self):
-        return f'User {self.username}'
-
-        _
+        return "User: %s" %str(self.username)
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.Text(),nullable=False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def get_blog(id):
+        post = post.query.filter_by(id=id).first()
+        return post
+    def __repr__(self):
+        return f'Blog {self.title}'
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Comment {self.comment}'
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Subscriber {self.email}'
